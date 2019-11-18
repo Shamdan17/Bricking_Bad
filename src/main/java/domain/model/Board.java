ce81cb5b399ce667c35c5a41eae511bac1b78920
Shadi Hamdan <shamdan17@ku.edu.tr>
2019-11-18 17:17:31 +0300

    add skeletons

---
+++ src/main/java/domain/model/Board.java
@@ -0,0 +1,12 @@
+package domain.model;
+
+import domain.Movable;
+
+import java.util.List;
+
+// Board contains all movables
+public class Board {
+    List<Movable> movables;
+
+
+}
---
+++ src/main/java/domain/model/CollisionRuleEngine.java
@@ -0,0 +1,9 @@
+package domain.model;
+
+import domain.Movable;
+
+public class CollisionRuleEngine {
+    public void collide(Movable obj1, Movable obj2){
+
+    }
+}
