3acaf6fe948cd7079f8b2726cdd6e7c022fc62a4
Mohammad Kefah <mo@edraj.io>
2019-11-21 10:54:24 +0300

    add initial  implementation of controller

--- src/main/java/domain/model/Ball.java
+++ src/main/java/domain/model/Ball.java
@@ -9,23 +9,30 @@ public class Ball extends Circle {
 
     public Ball(int radius) {
         super(radius);
+
+        // TODO we need to remove constants, also we need to check every place
+        // where we initialize variable, we don't want null thingies
+        // (for example by design any setter should not accept null, contractors
+        // should not accept null, etc..)
+        setVelocity(new Velocity(10, 10));
     }
 
     public void collide(MovableShape obj) {
-        //TODO Implement this
-//        if (obj.getType() == bottomWall) {
-//            destroy();
-//        }
+       // TODO Implement this
+       // if (obj.getType() == bottomWall) {
+       //     destroy();
+       // }
     }
 
     @Override
     public void move() {
         // Get parameters
+        // TODO probability of future null thingy here
         Position oldPos = getPosition();
-        Velocity velocity = getVelocity();
 
         // Calculate new position
-        Position newPos = oldPos.incrementX(velocity.getX()).incrementY(velocity.getY());
+        // Do we really need this (maybe we can provide oldPos.move(dx, dy)?
+        Position newPos = oldPos.incrementX(getVelocity().getX()).incrementY(getVelocity().getY());
         setPosition(newPos);
     }
 
