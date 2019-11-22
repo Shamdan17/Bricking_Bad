a9dc44bfa8a068c150796b476a91bb81df2b7952
Mohammad Kefah <mo@edraj.io>
2019-11-22 13:41:04 +0300

    finalize save manager

--- src/main/java/domain/storage/StorageManager.java
+++ src/main/java/domain/storage/StorageManager.java
@@ -5,4 +5,6 @@ import java.io.Serializable;
 public interface StorageManager {
    public void   put(Serializable key, Serializable value);
    public Object get(Serializable key);
+   public void save();
+   public void load();
 }
