43dc61c283815edbeceb4b5b46bcfcbe447c92ec
Shadi Hamdan <shamdan17@ku.edu.tr>
2019-12-06 12:44:31 +0300

    Make game slower

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
     public static final int PADDLE_WIDTH = 30;
     //todo width should be %10 of the screen width and not a constant
     public static final int L = maxX/6;
-    public static final long SLEEP_TIME = 5;
+    public static final long SLEEP_TIME = 10;
 
     // TODO: reorganize this file
     public static final int LENGTH = 60;
