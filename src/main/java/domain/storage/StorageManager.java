880c8473500f129e3eeb67cb34e67e03e9a2700e
Mohammad Kefah <mo@edraj.io>
2019-12-15 22:02:08 +0300

    fix documentation

--- src/main/java/domain/storage/StorageManager.java
+++ src/main/java/domain/storage/StorageManager.java
@@ -8,21 +8,24 @@ import java.io.Serializable;
  */
 public interface StorageManager {
 
-    /**
-     * @param key
-     * @param value
-     */
-    public void put(Serializable key, Serializable value);
+	/**
+	 *
+	 * @param key the identifier of saved data
+	 * @param value the actual data to be saved
+	 */
+    public void   put(Serializable key, Serializable value);
 
     /**
-     * @param key
-     * @return
+     *
+     * @param key the identifier of saved data
+     * @return the saved data associated with the given key
      */
     public Object get(Serializable key);
 
     /**
-     * @param key
-     * @return
+     *
+     * @param key the identifier of saved data
+     * @return a boolean indicating if an entry with the given key exists or not
      */
     public boolean contains(Serializable key);
 }
